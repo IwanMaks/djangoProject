90e8566409f23ed0fd671f3782822dd1a67cc1cd
prostochlen <iwan.maksackov@yandex.ru>
2020-12-12 15:57:04 +0300

    add static files

--- firstSite/news/views.py
+++ firstSite/news/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-from .models import News
+from .models import News, Category
 
 
 def index(request):
@@ -11,3 +10,14 @@ def index(request):
         'title': 'Список новостей'
     }
     return render(request, 'news/index.html', context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    cat = Category.objects.get(pk=category_id)
+    return render(request, 'news/category.html', {'news': news, 'category': cat})
+
+
+def view_news(request, news_id):
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
